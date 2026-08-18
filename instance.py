5b03f4b39adb1c103b430f3d834abfcfc63985de
Charantej-4 <Charan@fxmaster.co.uk>
2026-08-18 17:02:33 +0530

    add new python programs

---
+++ instance.py
@@ -0,0 +1,11 @@
+class student():
+    college="vbit"
+
+    def __init__(self,name):
+        self.name=name
+s1=student("tej")
+s2=student("sai")
+print(s1.name)
+print(s2.name)
+print(s1.college)
+print(s2.college)
